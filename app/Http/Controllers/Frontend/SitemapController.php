49a6daf4afcb4c038b5e5a070a09d05423724c85
serikabdikarimov <124126839+serikabdikarimov@users.noreply.github.com>
2023-11-15 21:43:49 +0600

    ss
    ss

--- app/Http/Controllers/Frontend/SitemapController.php
+++ app/Http/Controllers/Frontend/SitemapController.php
@@ -24,6 +24,22 @@ class SitemapController extends Controller
             'authors' =>$authors
         ])->header('Content-Type', 'text/xml');
     }
+
+    public function sitemapHtml()
+    {
+        $domainId = Localization::where('locale_name', request()->getHost())->first();
+        $homePage = Page::where('slug', '/')->where('locale_id', $domainId->id)->orderBy('updated_at', 'DESC')->first();
+        $pages = Page::where(['status' => 2,'locale_id' => $domainId->id, 'type' => 1])->where('slug', '<>', '/')->orderBy('updated_at', 'DESC')->get();
+        $brands = Page::where(['status' => 2,'locale_id' => $domainId->id, 'type' => 2])->orderBy('updated_at', 'DESC')->get();
+        //$authors = Page::where(['status' => 2,'locale_id' => $domainId->id, 'type' => 3])->orderBy('updated_at', 'DESC')->get();
+
+        return response()->view('frontend.sitemap.sitemap-html', [
+            'homePage' => $homePage,
+            'pages' => $pages,
+            'brands' => $brands,
+            //'authors' =>$authors
+        ]);
+    }
 }
 
 
